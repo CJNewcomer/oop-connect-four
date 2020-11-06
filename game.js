1483e4b2289fcc06d4a6f7b21def10d3ff221394
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2020-11-06 15:02:24 -0600

    determining the tie

--- game.js
+++ game.js
@@ -6,9 +6,13 @@ export class Game {
     this.player1Name = player1Name;
     this.player2Name = player2Name;
     this.currentPlayer = 1;
+    this.winnerNumber = 0;
     // console.log(this.columns);
   }
   getName() {
+    if (this.winnerNumber === 3) {
+      return `${this.player1Name} ties with ${this.player2Name}!`;
+    }
     return `${this.player1Name} vs. ${this.player2Name}`;
   }
   playInColumn(column) {
@@ -18,11 +22,26 @@ export class Game {
     } else {
         this.currentPlayer = 1;
     }
+    if (this.checkForTie()) {
+      this.winnerNumber = 3;
+    }
   }
   getTokenAt(row, column) {
     return this.columns[column].getTokenAt(row);
   }
   isColumnFull(column) {
     return this.columns[column].isFull();
+  }
+  checkForTie() {
+    for (let i = 0; i < this.columns.length; i++) {
+      if (!this.columns[i].isFull()) {
+        return false;
+      } 
+    }
+    return true;
+    // const fullColumns = this.columns.map((ele) => {
+    //   return ele.isFull();
+    // })
+    // return (!fullColumns.find(false));
   }
 }
